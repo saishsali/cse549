b4f69b8c702273afebe3fdd8cff20848d06d04ea
saishsali <salisaish@gmail.com>
2017-09-09 23:33:21 -0400

    Shortest common superstring

---
+++ shortest_superstring.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define SIZE 100
+
+int string_overlap(string s1, string s2, string &combined) {
+    int s1_len = s1.length(), s2_len = s2.length(), overlap_len = INT_MIN, i = 0, j = 0;
+
+    if (s1_len > s2_len) {
+        if (s1.find(s2) != string::npos) {
+            combined = s1;
+
+            return s2_len;
+        } else {
+            i = s1_len - s2_len;
+        }
+    } else if (s2_len > s1_len) {
+        if (s2.find(s1) != string::npos) {
+            combined = s2;
+
+            return s1_len;
+        } else {
+            j = s2_len - s1_len;
+        }
+    } else {
+        if (s1.compare(s2) == 0) {
+            combined = s1;
+
+            return s1_len;
+        }
+    }
+
+    while (i < s1_len) {
+        if (s1.compare(i, s1_len - i, s2, 0, s1_len - i) == 0) {
+            overlap_len = s1_len - i;
+            combined = s1 + s2.substr(overlap_len);
+            break;
+        }
+        i++;
+    }
+
+    while (j < s2_len) {
+        if (s2.compare(j, s2_len - j, s1, 0, s2_len - j) == 0) {
+            if ((s2_len - j) > overlap_len) {
+                overlap_len = s2_len - j;
+                combined = s2 + s1.substr(overlap_len);
+                break;
+            }
+        }
+        j++;
+    }
+
+    return overlap_len;
+}
+
+string shortest_superstring(string dna[], int num_dna) {
+    int i, j, overlap, max_overlap, max_left, max_right;
+    string combined, max_combined;
+
+    while (num_dna != 1) {
+        max_overlap = INT_MIN;
+
+        for (int i = 0; i < num_dna - 1; i++) {
+            for (int j = i + 1; j < num_dna; j++) {
+                overlap = string_overlap(dna[i], dna[j], combined);
+                if (overlap > max_overlap) {
+                    max_overlap = overlap;
+                    max_combined = combined;
+                    max_left = i;
+                    max_right = j;
+                }
+            }
+        }
+
+        num_dna--;
+        if (max_overlap == INT_MIN) {
+            dna[0] += dna[num_dna];
+        } else {
+            dna[max_left] = max_combined;
+            dna[max_right] = dna[num_dna];
+        }
+    }
+
+    return dna[0];
+}
+
+int main() {
+    int T, num_dna;
+    string dna[SIZE], str;
+
+    cin >> T;
+
+    while (T--) {
+        cin >> num_dna;
+
+        int i = 0;
+        while (i < num_dna) {
+            cin >> dna[i++];
+        }
+
+        cout << shortest_superstring(dna, num_dna) << endl;
+    }
+    return 0;
+}
