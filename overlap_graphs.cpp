a326e33c597cf97b728a375ae129d222acd974ce
saishsali <salisaish@gmail.com>
2017-09-11 13:28:09 -0400

    Take k as input

--- overlap_graphs.cpp
+++ overlap_graphs.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 #define SIZE 100
 
-int dna_overlap(string s1, string s2) {
+int dna_overlap(string s1, string s2, int k) {
     int i, j, s1_len, s2_len, overlap;
 
     s1_len = s1.length();
     s2_len = s2.length();
 
-    if (s1.compare(s1_len - 4, 4, s2, 0, 4) == 0) // Check for match of 4 or more characters
+    if (s1.compare(s1_len - (k + 1), k + 1, s2, 0, k + 1) == 0) // Check for match of 4 or more characters
         return 0;
 
-    if (s1.compare(s1_len - 3, 3, s2, 0, 3) == 0) // Check for match of 3 characters
+    if (s1.compare(s1_len - k, k, s2, 0, k) == 0) // Check for match of 3 characters
         return 1;
 
     return 0;
 }
 
-void adjacency_list(string dna[], string rosalind[], int num_dna) {
+void adjacency_list(string dna[], string rosalind[], int num_dna, int k) {
     int overlap;
 
     for (int i = 0; i < num_dna; i++) {
         for (int j = 0; j < num_dna; j++) {
             if (i == j)
                 continue;
-            overlap = dna_overlap(dna[i], dna[j]);
+            overlap = dna_overlap(dna[i], dna[j], k);
 
             if (overlap == 1) {
                 cout << rosalind[i] << " " << rosalind[j] << endl;
@@ -35,20 +35,20 @@ void adjacency_list(string dna[], string rosalind[], int num_dna) {
 }
 
 int main() {
-    int T, num_dna;
+    int T, num_dna, k;
     string dna[SIZE], rosalind[SIZE];
 
     cin >> T; // number of test cases
 
     while (T--) {
-        cin >> num_dna;
+        cin >> num_dna >> k;
 
         for (int i = 0; i < num_dna; i++) {
             cin >> rosalind[i];
             cin >> dna[i];
         }
 
-        adjacency_list(dna, rosalind, num_dna);
+        adjacency_list(dna, rosalind, num_dna, k);
     }
 
     return 0;
